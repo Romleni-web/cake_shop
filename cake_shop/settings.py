8b93220a92ff76965f86bf8561ba882c88d20e5b
Romleni-web <romlenijohnson@gmail.com>
2026-07-22 14:09:02 +0300

    Fix: Force Postgres usage if DATABASE_URL is present

--- cake_shop/settings.py
+++ cake_shop/settings.py
@@ -60,13 +60,19 @@ WSGI_APPLICATION = 'cake_shop.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
