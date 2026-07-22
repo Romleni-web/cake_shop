c62a4b0fbc749f94fed08525cd2658b5ef1f68e9
Romleni-web <romlenijohnson@gmail.com>
2026-07-22 14:01:45 +0300

    Complete Melanin Cake House with M-Pesa and Rebranding

--- cake_shop/settings.py
+++ cake_shop/settings.py
@@ -58,11 +58,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cake_shop.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
